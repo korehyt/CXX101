b59923c27df043c114bf94d97f8f054e58ff143c
Koreh <90560605+korehyt@users.noreply.github.com>
2021-09-12 18:15:27 +0200

    Create 1 - Comments.cpp

---
+++ 1 - Introduction to C++/exercises/1 - Comments.cpp
@@ -0,0 +1,6 @@
+#include <iostream>                               // Include the header <iostream> so that we may write on the screen
+
+int main(int argc, const char* argv[]) {          // Declare the main function, this is the start point for our code
+    std::cout << "Hello, world!" << std::endl;    // Write "Hello, world!" on screen, followed by a line break
+    return 0;                                     // Return 0 to indicate that our program ran successfully
+}                                                 // This curly brace indicates the end of the main function
